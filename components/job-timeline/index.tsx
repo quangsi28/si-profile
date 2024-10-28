6390026e81f6867dd5d0755c90fe79c05a423117
Si Dang <quangsi28@gmail.com>
2024-10-28 15:01:12 +0700

    Create new job timeline component and append to bottom of sections
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/quangsi28/si-profile?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ components/job-timeline/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Box, Divider, Heading, Text, VStack } from '@chakra-ui/react';
+import { UiTimeline } from '../ui';
+
+interface Props {}
+
+const JobTimeline: React.FC<Props> = ({}) => {
+  const timelineItems = [
+    {
+      date: '2017',
+      title: 'FPT Software',
+      subTitle: 'Hanoi',
+      description: 'Add description',
+    },
+    {
+      date: '2019',
+      title: 'F99',
+      subTitle: 'Hanoi',
+      description: 'Add description',
+    },
+    {
+      date: '2020-present',
+      title: 'Selly',
+      subTitle: 'Hanoi',
+      description: 'Add description',
+    },
+  ];
+
+  return (
+    <Box id='jobTimelineSection' px={4} py={20} bg='gray.700' display='flex' flexDirection='column' alignItems='center'>
+      <VStack>
+        <Heading color='white'>Job Timeline</Heading>
+        <Text color='white'>More detail about job</Text>
+      </VStack>
+      <Divider mt='2' height='1px' w='300px' opacity='1' background='white' />
+      <UiTimeline items={timelineItems} />
+    </Box>
+  );
+};
+
+export default JobTimeline;
